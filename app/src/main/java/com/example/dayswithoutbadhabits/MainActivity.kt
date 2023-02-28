d6c1a0fb74f1266a9268d9bb14defdcfd92dc177
iolchu <ramil.iolchu@gmail.com>
2023-02-28 09:56:41 +0600

    added main functionality

--- app/src/main/java/com/example/dayswithoutbadhabits/MainActivity.kt
+++ app/src/main/java/com/example/dayswithoutbadhabits/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.dayswithoutbadhabits
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.dayswithoutbadhabits.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val binding by viewBinding(ActivityMainBinding::bind)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val sharedPref = getSharedPreferences("main", Context.MODE_PRIVATE)
+        val time = sharedPref.getLong("time", -1)
+        if (time == -1L) {
+            sharedPref.edit().putLong("time", System.currentTimeMillis()).apply()
+            binding.days.text = "0"
+            binding.reset.visibility = View.GONE
+        } else {
+            val days = (System.currentTimeMillis() - time) / (24 * 60 * 60 * 1000)
+            if (days == 0L) binding.reset.visibility = View.GONE
+            binding.days.text = days.toString()
+        }
+        binding.reset.setOnClickListener {
+            sharedPref.edit().putLong("time", System.currentTimeMillis()).apply()
+            binding.days.text = "0"
+            binding.reset.visibility = View.GONE
+        }
     }
+
 }
