f9a75b35c11e9e121ce23b91815e7454247e8bbb
iolchu <ramil.iolchu@gmail.com>
2023-03-01 09:40:25 +0600

    added realization for ui state

--- app/src/main/java/com/example/dayswithoutbadhabits/UiState.kt
+++ app/src/main/java/com/example/dayswithoutbadhabits/UiState.kt
@@ -1,8 +1,25 @@
 package com.example.dayswithoutbadhabits
 
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
 sealed class UiState {
 
-    object ZeroDays : UiState()
-    class NDays(private val days: Int) : UiState()
+    abstract fun apply(daysTextView: TextView, resetButton: Button)
+
+    object ZeroDays : UiState() {
+        override fun apply(daysTextView: TextView, resetButton: Button) {
+            daysTextView.text = "0"
+            resetButton.visibility = View.GONE
+        }
+    }
+
+    class NDays(private val days: Int) : UiState() {
+        override fun apply(daysTextView: TextView, resetButton: Button) {
+            daysTextView.text = days.toString()
+            resetButton.visibility = View.VISIBLE
+        }
+    }
 
 }
