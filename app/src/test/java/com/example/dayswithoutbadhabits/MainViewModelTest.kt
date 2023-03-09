80d7215c93eb5c0a956f7e28d845191425e228fc
iolchu <ramil.iolchu@gmail.com>
2023-03-09 10:16:42 +0600

    write new tests

--- app/src/test/java/com/example/dayswithoutbadhabits/MainViewModelTest.kt
+++ app/src/test/java/com/example/dayswithoutbadhabits/MainViewModelTest.kt
@@ -1,102 +1,120 @@
 package com.example.dayswithoutbadhabits
 
 import androidx.lifecycle.LifecycleOwner
-import com.example.dayswithoutbadhabits.main.presentation.MainCommunication
-import com.example.dayswithoutbadhabits.main.data.MainRepository
-import com.example.dayswithoutbadhabits.main.presentation.MainViewModel
-import com.example.dayswithoutbadhabits.main.presentation.UiState
 import org.junit.Before
 import org.junit.Test
-import kotlin.properties.Delegates
+import kotlin.test.assertEquals
 
 class MainViewModelTest {
 
-    private lateinit var repository: FakeRepository
+    private lateinit var viewModel: NewMainViewModel
+    private lateinit var interactor: FakeInteractor
     private lateinit var communication: FakeMainCommunication
-    private lateinit var viewModel: MainViewModel
 
     @Before
     fun init() {
-        repository = FakeRepository()
-        communication = FakeMainCommunication.Base()
-        viewModel = MainViewModel(repository, communication)
+        viewModel = NewMainViewModel()
+        interactor = FakeInteractor(listOf(Card.Add))
+        communication = FakeMainCommunication(interactor, communication)
     }
 
     @Test
-    fun test_zero_days() {
-        repository.setFakeDays(0)
-        viewModel.init(isFirstRun = true)
-        assert(communication.checkCalledCount(1))
-        assert(communication.isSame(UiState.ZeroDays))
-        viewModel.init(isFirstRun = false)
-        assert(communication.checkCalledCount(1))
+    fun start() {
+        viewModel.init(isFirstRun == true)
+        assertEquals(NewUiState.Add(Card.Add), communication.list[0])
+        assertEquals(1, communication.list.size)
     }
 
     @Test
-    fun test_n_days() {
-        repository.setFakeDays(5)
-        viewModel.init(isFirstRun = true)
-        assert(communication.checkCalledCount(1))
-        assert(communication.isSame(UiState.NDays(days = 5)))
-        viewModel.init(isFirstRun = false)
-        assert(communication.checkCalledCount(1))
+    fun add_first_card() {
+        viewModel.init(isFirstRun == true)
+        assertEquals(NewUiState.Add(Card.Add), communication.list[0])
+        assertEquals(1, communication.list.size)
+
+        viewModel.addCard(position = 0)
+        assertEquals(NewUiState.Replace(position = 0, Card.Make), communication.list[0])
+        assertEquals(2, communication.list.size)
     }
 
     @Test
-    fun test_reset() {
-        repository.setFakeDays(5)
-        viewModel.init(isFirstRun = true)
-        assert(communication.checkCalledCount(1))
-        assert(communication.isSame(UiState.NDays(days = 5)))
-        viewModel.reset()
-        assert(communication.checkCalledCount(2))
-        assert(communication.isSame(UiState.ZeroDays))
-        assert(repository.checkResetCalledCount(1))
+    fun cancel_make_card() {
+        viewModel.init(isFirstRun == true)
+        assertEquals(NewUiState.Add(Card.Add), communication.list[0])
+        assertEquals(1, communication.list.size)
+
+        viewModel.addCard(position = 0)
+        assertEquals(NewUiState.Replace(position = 0, Card.Make), communication.list[1])
+        assertEquals(2, communication.list.size)
+
+        viewModel.cancelMakeCard(position = 0)
+        assertEquals(NewUiState.Replace(position = 0, Card.Add), communication.list[2])
+        assertEquals(3, communication.list.size)
+    }
+
+    @Test
+    fun save_first_card() {
+        viewModel.init(isFirstRun == true)
+        assertEquals(NewUiState.Add(Card.Add), communication.list[0])
+        assertEquals(1, communication.list.size)
+
+        viewModel.addCard(position = 0)
+        assertEquals(NewUiState.Replace(position = 0, Card.Make), communication.list[1])
+        assertEquals(2, communication.list.size)
+
+        interactor.canAddNewCard = true
+        viewModel.saveNewCard(text = "days without smoking", position = 0)
+        assertEquals(NewUiState.Replace(position = 0, Card.ZeroDays(text = "days without smoking")), communication.list[2])
+        assertEquals(NewUiState.Add(Card.Add), communication.list[3])
+        assertEquals(4, communication.list.size)
+        assertEquals(1, interactor.canAddNewCardCalledCount)
+        assertEquals(true, interactor.canAddNewCard)
+    }
+
+    @Test
+    fun save_only_one_card() {
+        viewModel.init(isFirstRun == true)
+        assertEquals(NewUiState.Add(Card.Add), communication.list[0])
+        assertEquals(1, communication.list.size)
+
+        viewModel.addCard(position = 0)
+        assertEquals(NewUiState.Replace(position = 0, Card.Make), communication.list[1])
+        assertEquals(2, communication.list.size)
+
+        interactor.canAddNewCard = false
+        viewModel.saveNewCard(text = "days without smoking", position = 0)
+        assertEquals(NewUiState.Replace(position = 0, Card.ZeroDays(text = "days without smoking")), communication.list[2])
+        assertEquals(3, communication.list.size)
+        assertEquals(1, interactor.canAddNewCardCalledCount)
+        assertEquals(false, interactor.canAddNewCard)
+    }
+
+
+}
+
+private class FakeInteractor(
+    private val cards: List<Card>
+) : NewMainInteractor() {
+
+    var canAddNewCard = true
+    var canAddNewCardCalledCount = 0
+
+    override fun cards(): List<Card> = cards
+
+    override fun canAddNewCard(): Boolean {
+        canAddNewCardCalledCount++
+        return canAddNewCard
     }
 
 }
 
-private class FakeRepository : MainRepository {
+private class FakeMainCommunication : NewMainCommunication {
 
-    private var days by Delegates.notNull<Int>()
-    private var resetCalledCount = 0
+    val list = mutableListOf<NewUiState>()
 
-    override fun days() = days
-
-    fun setFakeDays(days: Int) {
-        this.days = days
+    override fun put(value: NewUiState) {
+        list.add(value)
     }
 
-    fun checkResetCalledCount(count: Int): Boolean = resetCalledCount == count
-
-    override fun reset() {
-        resetCalledCount++
-    }
-
-}
-
-private interface FakeMainCommunication : MainCommunication {
-
-    fun checkCalledCount(count: Int): Boolean
-    fun isSame(state: UiState): Boolean
-
-    class Base : FakeMainCommunication {
-
-        private lateinit var state: UiState
-        private var putCallesCount = 0
-
-        override fun checkCalledCount(count: Int) = putCallesCount == count
-
-        override fun isSame(state: UiState) = this.state == state
-
-        override fun observe(owner: LifecycleOwner, action: (UiState) -> Unit) = Unit
-
-        override fun put(value: UiState) {
-            putCallesCount++
-            state = value
-        }
-
-    }
-
+    override fun observe(owner: LifecycleOwner, , action: (NewUiState) -> Unit)
 
 }
